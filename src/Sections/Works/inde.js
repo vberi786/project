c6993e3cb2877807ad6a36b207c2048249c78848
vberi786 <100026333+vberi786@users.noreply.github.com>
2023-01-05 19:41:46 +0530

    done

---
+++ src/Sections/Works/inde.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import sy from "./sy.css"
+const inde = () => {
+  return (
+    <div>kida our works</div>
+  )
+}
+
+export default inde
---
+++ src/Sections/recentpro/Recent.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import recent from "../recentpro/recent.css"
+const Recent = () => {
+  return (
+
+    <div class="wrapper">
+    <h1 class="foun">Our Founders</h1>
+    <div class="team">
+      <div class="team_member">
+        <div class="team_img">
+        <img class="vasu" src=""></img>
+         
+        </div>
+        <h3>Vasu Beri</h3>
+        <p class="role">MERN Developer</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p>
+      
+      </div>
+      <div class="team_member">
+        <div class="team_img">
+         <img class="vasu" src=""></img>
+        </div>
+        <h3>Vasu Patidar</h3>
+        <p class="role">Web Developer </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p></div>
+      
+    </div>
+  </div>
+  )
+}
+
+export default Recent
